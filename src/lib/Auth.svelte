04e1843fd05bc22ecb41b7e056ea47235b339320
rachensp <80815336+rachensp@users.noreply.github.com>
2024-03-07 21:26:15 +0100

    fixing

--- src/lib/Auth.svelte
+++ src/lib/Auth.svelte
@@ -3,16 +3,23 @@
 
 	let loading = false;
 	let email = '';
+	let showModal = false;
+	let modalMessage = '';
+
+	const showModalAlert = (message: string) => {
+		modalMessage = message;
+		showModal = true;
+	};
 
 	const handleLogin = async () => {
 		try {
 			loading = true;
 			const { error } = await supabase.auth.signInWithOtp({ email });
 			if (error) throw error;
-			alert('Check your email for login link!');
+			showModalAlert('Check your email for login link!');
 		} catch (error) {
 			if (error instanceof Error) {
-				alert(error.message);
+				showModalAlert(error.message);
 			}
 		} finally {
 			loading = false;
@@ -43,3 +50,10 @@
 		</form>
 	</div>
 </div>
+
+<DaisyModal bind:show={showModal}>
+	<div class="p-4">
+		<h1 class="text-2xl font-bold">{modalMessage}</h1>
+		<button class="mt-4 button block" on:click={() => (showModal = false)}>Close</button>
+	</div>
+</DaisyModal>
