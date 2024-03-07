753b8ef97eef2591b439e43c8137f8862ea98da4
rachensp <80815336+rachensp@users.noreply.github.com>
2024-03-07 21:55:24 +0100

    fixes

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,15 +3,14 @@
 </script>
 
 <div class="flex justify-center items-center min-h-screen">
-	<div
-		class="w-full md:max-w-md p-8 bg-white rounded-lg shadow-md top-quarter flex justify-center items-center"
-	>
+	<div class="w-full md:w-3/4 lg:w-1/2 xl:w-1/3 p-8 bg-white rounded-xl shadow-xl text-black">
 		<Auth />
 	</div>
 </div>
 
 <style>
-	.top-quarter {
-		margin-top: 12.5vh;
+	.text-black {
+		--tw-text-opacity: 1;
+		color: rgba(0, 0, 0, var(--tw-text-opacity));
 	}
 </style>
