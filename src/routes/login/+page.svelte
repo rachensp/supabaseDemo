e9e6e11be6a7befdb30dfbd0bd7b96ea1cf7abfb
rachensp <80815336+rachensp@users.noreply.github.com>
2024-03-18 10:21:29 +0100

    news

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex justify-center items-center min-h-screen">
-	<div class="w-full md:w-3/4 lg:w-1/2 xl:w-1/3 p-8 bg-white rounded-xl shadow-xl text-black">
+	<div class="w-full md:w-3/4 lg:w-1/2 xl:w-1/3 p-8 bg-white rounded-xl shadow-xl text-black auth">
 		<Auth />
 	</div>
 </div>
@@ -12,5 +12,10 @@
 	.text-black {
 		--tw-text-opacity: 1;
 		color: rgba(0, 0, 0, var(--tw-text-opacity));
+	}
+
+	/* Style für das E-Mail-Input-Feld innerhalb von <Auth /> */
+	:global(.auth input[type='email']) {
+		background-color: white !important; /* Hintergrundfarbe auf Weiß setzen */
 	}
 </style>
